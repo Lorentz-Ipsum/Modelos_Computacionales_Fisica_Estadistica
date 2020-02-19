8cb19f479e21d5174e03ed308c96200339340693
Lorentz Ipsum <manu.f.arroyo@gmail.com>
2020-02-19 19:21:34 +0100

    Created Python files

---
+++ Python_Scripts/01-limite_central.py
@@ -0,0 +1,6 @@
+# Objetivos:
+# 1. Generar N numeros aleatorios con media Mu y varianza Sigma
+# 2. Construir Y = X_1 + X_2 +...+ X_N
+# 3. Calcular P_Y (y)
+
+datos = 
---
+++ Python_Scripts/02-kac.py
@@ -0,0 +1,6 @@
+# Objetivos:
+# 1. Generar N casillas
+# 2. Disponer M sitios al azar
+# 3. Establecer cc periodicas
+# 4. Establecer ley de cambio de casilla al pasar por un sitio
+# 5. Mover casillas
